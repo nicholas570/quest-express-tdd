4d43e0a2f45bb93954716eefaa8106317bd55d09
nicolas <nicolas.mercier570@gmail.com>
2021-02-08 15:33:16 +0100

    Tested routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // app.js
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
+const connection = require('./connection');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,6 +10,46 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello World!' });
+});
+
+app.get('/bookmarks/:id', (req, res) => {
+  connection.query(
+    'SELECT * FROM bookmark WHERE id = ?',
+    [req.params.id],
+    (err, result) => {
+      if (err) {
+        return res.status(500).json({ err });
+      }
+
+      if (result.length === 0) {
+        return res.status(404).json({ error: 'Bookmark not found' });
+      }
+
+      return res.status(200).json(result[0]);
+    }
+  );
+});
+
+app.post('/bookmarks', (req, res) => {
+  if (!req.body.url || !req.body.title) {
+    return res.status(422).json({ error: 'required field(s) missing' });
+  }
+  connection.query('INSERT INTO bookmark SET ?', [req.body], (err, result) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+
+    return connection.query(
+      'SELECT * FROM bookmark WHERE id = ?',
+      [result.insertId],
+      (error, records) => {
+        if (err) {
+          return res.status(500).json(error);
+        }
+        return res.status(201).json(records[0]);
+      }
+    );
+  });
 });
 
 module.exports = app;
--- test/app.integration.test.js
+++ test/app.integration.test.js
@@ -1,7 +1,11 @@
 const request = require('supertest');
 const app = require('../app');
+const connection = require('../connection');
 
 describe('Test routes', () => {
+  beforeEach((done) => connection.query('TRUNCATE bookmark', done));
+
+  // GET hello world
   it('GET / sends "Hello World" as json', (done) => {
     request(app)
       .get('/')
@@ -12,5 +16,78 @@ describe('Test routes', () => {
         expect(response.body).toEqual(expected);
         done();
       });
+  });
+
+  // POST a bookmark fail
+  it('POST / Error sends create data as json', (done) => {
+    request(app)
+      .post('/bookmarks')
+      .send({})
+      .expect(422)
+      .expect('Content-Type', /json/)
+      .then((response) => {
+        const expected = { error: 'required field(s) missing' };
+        expect(response.body).toEqual(expected);
+        done();
+      });
+  });
+
+  // POST a bookmark success
+  it('POST / Success sends create data as json', (done) => {
+    request(app)
+      .post('/bookmarks')
+      .send({ url: 'https://jestjs.io', title: 'Jest' })
+      .expect(201)
+      .expect('Content-Type', /json/)
+      .then((response) => {
+        const expected = {
+          id: expect.any(Number),
+          url: 'https://jestjs.io',
+          title: 'Jest',
+        };
+        expect(response.body).toEqual(expected);
+        done();
+      })
+      .catch(done);
+  });
+
+  // GET a specific bookmark
+  describe('GET /bookmarks/:id', () => {
+    const testBookmark = { url: 'https://nodejs.org/', title: 'Node.js' };
+    beforeEach((done) =>
+      connection.query('TRUNCATE bookmark', () =>
+        connection.query('INSERT INTO bookmark SET ?', testBookmark, done)
+      )
+    );
+
+    // Fail
+    it('GET / Error no bookmarks for this id', (done) => {
+      request(app)
+        .get(`/bookmarks/${354}`)
+        .expect(404)
+        .expect('Content-Type', /json/)
+        .then((response) => {
+          const expected = { error: 'Bookmark not found' };
+          expect(response.body).toEqual(expected);
+          done();
+        });
+    });
+
+    // Success
+    it('GET / Success bookmarks for this id', (done) => {
+      request(app)
+        .get(`/bookmarks/${1}`)
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .then((response) => {
+          const expected = {
+            id: expect.any(Number),
+            url: 'https://nodejs.org/',
+            title: 'Node.js',
+          };
+          expect(response.body).toEqual(expected);
+          done();
+        });
+    });
   });
 });
